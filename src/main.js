bd75335e2bf907fff42ed582a4dc51e54f2378ce
SolangeLopez <solangelopezdev@gmail.com>
2023-08-03 16:09:43 -0300

    add finish

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 import { createApp } from 'vue'
-import './style.css'
+import {createWebHistory, createRouter} from 'vue-router'
+import Home from './components/Home.vue'
+import CategoryDetails from './components/CategoryDetails.vue'
+import MealDetails from './components/MealDetails.vue'
 import App from './App.vue'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap' 
 
-createApp(App).mount('#app')
+
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/category/:title",
+        name: "Category",
+        component: CategoryDetails
+    },
+    {
+        path: "/meal/:id",
+        name: "Meal",
+        component: MealDetails
+    }, 
+
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
+createApp(App).use(router).mount('#app')
